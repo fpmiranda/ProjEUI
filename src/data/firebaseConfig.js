c5a0fc42004c14eb7783fbd81e4e7d6c8f3e4a08
Felipe Miranda <felipe300194miranda@gmail.com>
2023-08-12 15:40:10 -0300

    Add files via upload
    arquivo de configuração do firebase

---
+++ src/data/firebaseConfig.js
@@ -0,0 +1,16 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
